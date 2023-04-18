427aeee2c85d9334b7bee451acb15f0fd480748b
Federico Crespi <crespif@celta.com.ar>
2023-04-18 13:58:43 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { List, ListItem, ListItemAvatar, ListItemText } from '@mui/material'
+
+import PersonIcon from '@mui/icons-material/Person';
+import EventIcon from '@mui/icons-material/Event';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import EmailIcon from '@mui/icons-material/Email';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,107 +21,47 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        <div className={styles.personalDate}>
+          <div className={styles.contentImageProfile}>
+            <Image className={styles.imageProfile} src="/profile.jpg" alt="profile" width={200} height={200} />
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div className={styles.contentDate}>
+            <h1 className={styles.name}>Detalles</h1>
+            <h1>Personales</h1>
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <PersonIcon />
+                </ListItemAvatar>
+                <ListItemText primary="Nombre" secondary="Crespi Federico" />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <EventIcon />
+                </ListItemAvatar>
+                <ListItemText primary="Fecha Nacimiento" secondary="18/08/1990" />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <SmartphoneIcon />
+                </ListItemAvatar>
+                <ListItemText primary="Telefono" secondary="54 2983 655609" />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <EmailIcon />
+                </ListItemAvatar>
+                <ListItemText primary="Email" secondary="fdcrespi@gmail.com" />
+              </ListItem>
+            </List>
           </div>
+          <div className={styles.contentIdiom}>
+            <h1>Idiomas</h1>
+          </div>
+        
         </div>
+        <div className={styles.resumeDate}>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
